e63c0aa677d845ba4b477419c4011ca80f34b534
Ap73MKa <artyom.vlasov.05@mail.ru>
2024-12-17 00:26:22 +0300

    fix: not showing toast messages, android application name

--- apps/mobile/src/features/auth/sign-out-button.svelte
+++ apps/mobile/src/features/auth/sign-out-button.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
   import { Button } from '@repo/ui/button'
 
-  import { user } from '@/shared/auth';
+  import { logout, user } from '@/shared/auth';
 
   type Props = {
     children?: import('svelte').Snippet;
-    [key: string]: any
+    [key: string]: unknown
   }
 
   let { children, ...rest }: Props = $props();
 
-  const signOut = () => { user.set(null) }
+  const signOut = async () => {
+    user.set(null)
+    await logout($user?.token ?? "")
+  }
 </script>
 
 <Button {...rest} on:click={signOut}>
